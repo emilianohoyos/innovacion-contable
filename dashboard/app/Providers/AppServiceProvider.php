d320389df7eb07174f3d88a07251fc7af7c6d865
Emiliano Hoyos <emilianohoyos10@gmail.com>
2025-01-10 18:17:37 -0500

    se realiza cambio en forzar https

--- dashboard/app/Providers/AppServiceProvider.php
+++ dashboard/app/Providers/AppServiceProvider.php
@@ -23,6 +23,8 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         Schema::defaultStringLength(191);
-        URL::forceScheme('https');
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
